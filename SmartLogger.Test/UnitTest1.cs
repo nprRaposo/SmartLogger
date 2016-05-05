760a2bb7b53f8c53a5e15e429cbecfbbb369ef8c
nraposo <niko.rps@hotmail.com>
2016-05-05 17:39:40 -0300

    Primera versión del Smart Logger

---
+++ SmartLogger.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartLogger.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
